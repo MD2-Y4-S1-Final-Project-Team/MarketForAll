36e6491fa1dae87bd375b03ad11c9272f0ab6965
Saingchakkrey <saingchakkreymeas@gmail.com>
2019-02-11 15:04:01 +0700

    Email And Password Sign In and Up

--- app/src/main/java/com/example/asus/marketforall/MainActivity.java
+++ app/src/main/java/com/example/asus/marketforall/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.asus.marketforall;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private static final FirebaseAuth auth = FirebaseAuth.getInstance();
+    private static final FirebaseUser user = auth.getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alreadyLogin();
+    }
+
+    private void alreadyLogin() {
+        if(user == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
